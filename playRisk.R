bff43a1cd9f50983f69cf777f14fd3cc7b291f6c
Peter Phalen <peterphalen@Peters-MacBook-Pro.local>
2019-05-16 23:17:45 -0400

    replace = TRUE

--- playRisk.R
+++ playRisk.R
@@ -1,26 +1,26 @@
 
 
 
-playRisk <- function( attack.dice = 3, # num. of attack dice
-                      defend.dice = 2, # num. of defend dice
-                      rolls = 100 # num. of simulations
+playRisk <- function( rolls = 100, # num. of simulations
+                      attack.dice = 3, # num. of attack dice
+                      defend.dice = 2 # num. of defend dice
                       ){
   
-  attacker.total.kills <- 0 #attacker kills across rolls
+  attacker.kill.count <- 0 #attacker kills across rolls
   total.casualties <- 0 # number of soldiers lost across rolls
   
   for ( roll in 1:rolls ) {
   
   # roll the dice
-  a.rolls <- sample(1:6, size = attack.dice )
-  d.rolls <- sample(1:6, size = defend.dice )
+  a.rolls <- sample(1:6, size = attack.dice, replace=TRUE)
+  d.rolls <- sample(1:6, size = defend.dice, replace=TRUE)
   
   # order the values from highest to lowest
   a.rolls <- a.rolls[order(a.rolls, decreasing = TRUE)]
   d.rolls <- d.rolls[order(d.rolls, decreasing = TRUE)]
   
   # number of comparisons needed to score
-  # is same as the lowest num. of die between attacker
+  # is same as the lowest num. of dice between attacker
   # and defender
   comparisons <- min( attack.dice, defend.dice ) 
   total.casualties <- total.casualties + comparisons
@@ -37,22 +37,22 @@ playRisk <- function( attack.dice = 3, # num. of attack dice
   }
   
   # add result of this roll to the tally 
-  attacker.total.kills <- attacker.total.kills + attacker.kills.this.roll
+  attacker.kill.count <- attacker.kill.count + attacker.kills.this.roll
 
   }
   
-  attacker.casualties <- total.casualties - attacker.total.kills
+  attacker.casualties <- total.casualties - attacker.kill.count
 
-  
   # total attacker kills / total possible kills = % success for attacker
-  percent.attacker.kills <- (attacker.total.kills / total.casualties)
+  attacker.kills <- (attacker.kill.count / total.casualties)
   #format as percent
-  percent.attacker.kills <-   paste0(formatC(100 * percent.attacker.kills, format = "f", digits = 2), "%")
-  
-  # kills:deaths ratio
-  kill.death.ratio <- round( ( attacker.total.kills / attacker.casualties ), digits = 2)
+  attacker.kills <-   paste0(formatC(100 * attacker.kills, format = "f", digits = 2), "%")
 
-  
-  return (  data.frame(attacker.total.kills, attacker.casualties, percent.attacker.kills, kill.death.ratio)  )
+  # kills:deaths ratio
+  kill.death.ratio <- round( ( attacker.kill.count / attacker.casualties ), digits = 2)
+
+  return (  data.frame(attacker.kill.count, attacker.casualties, attacker.kills, kill.death.ratio)  )
   
 }
+
+
